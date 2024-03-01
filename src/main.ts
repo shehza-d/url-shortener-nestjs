971c3617f27a316b153fe82a6c564b3d77520ff9
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-03-01 23:58:35 +0500

    users MVC

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const PORT = process.env.PORT || 3003;
+
+  app.enableCors();
+
+  await app.listen(PORT);
 }
 bootstrap();
