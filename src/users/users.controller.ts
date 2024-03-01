7177ab2e6ec2fa33b08d02c56f498daceeb5124a
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2024-03-02 01:06:05 +0500

    basic UsersController setup

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,41 @@
-import { Controller } from "@nestjs/common";
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Delete,
+  Query,
+} from "@nestjs/common";
 
 @Controller("users")
-export class UsersController {}
+export class UsersController {
+  //
+
+  @Get() //  /users
+  findAll(@Query("role") role?: string) {
+    console.log("🚀 ~ UsersController ~ findAll ~ role:", role);
+    return [];
+  }
+
+  @Get(":id") // /users/01
+  findOneUser(@Param("id") id: string) {
+    return { id };
+  }
+
+  @Post() // post /users
+  createUser(@Body() user: object) {
+    return user;
+  }
+
+  @Patch() //  /users
+  updateUser(@Param("id") id: string, @Body() userUpdate: object) {
+    return { id, ...userUpdate };
+  }
+
+  @Delete(":id") //  /users/1
+  deleteUser(@Param("id") id: string) {
+    return { id };
+  }
+}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
 import { UsersController } from "./users.controller";
+import { UsersService } from "./users.service";
 
 @Module({
   controllers: [UsersController],
-  providers: [],
+  providers: [UsersService],
 })
 export class UsersModule {}
